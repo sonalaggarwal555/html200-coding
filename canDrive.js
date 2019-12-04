08a2d9e6eb95718830288bee86f62acfbb75305b
sonalaggarwal555 <46764998+sonalaggarwal555@users.noreply.github.com>
2019-12-04 15:55:08 -0800

    Lesson 2 Assignment 4 canDrive.js

---
+++ canDrive.js
@@ -0,0 +1,14 @@
+let fname = 'sonal';
+let lname = 'fnu';
+const age = 16;
+const drivingage = 18;
+if(age<drivingage)
+{
+    for(let i=5;i>0;i--){
+      console.log('i: '+i);
+    }
+}
+  else
+  {
+  console.log(fname+lname+'you can drive');
+}
