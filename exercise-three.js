899b69da23735133028beafdf70b18798457581f
sonalaggarwal555 <46764998+sonalaggarwal555@users.noreply.github.com>
2019-12-22 12:59:13 -0800

    Lesson-3 Assignment-2 Regular Challenge 2

--- exercise-three.js
+++ exercise-three.js
@@ -1,3 +1,9 @@
+/*The sentence is: I Love Mangoes And Strawberries!*/
+string_to_array = function(str){
+  return str.trim().split(" ");
+};
+/*Array of String*/
+let arr = string_to_array("I Love Mangoes And Strawberries !");
 let arr = ['Vanilla','Strawberry','Butterscotch','Chocolate','Caramel'];
 for(let i=0;i<arr.length;i++){
   console.log(i+','+arr[i]);
--- exercise-two.js
+++ exercise-two.js
@@ -4,7 +4,7 @@ string_to_array = function(str){
   return str.trim().split(" ");
 };
 /*Array of String*/
-let arr = string_to_array("I Love Mangoes And Strawberries!");
+let arr = string_to_array("I Love Mangoes And Strawberries !");
 
 /*Third word in array*/
 console.log('Third word in array: '+arr[2]);
