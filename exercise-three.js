01a4866395b6208fb06c2cf8359d4b04cd432f6a
sonalaggarwal555 <46764998+sonalaggarwal555@users.noreply.github.com>
2019-12-22 13:02:34 -0800

    Lesson-3 Assignment-3 Regular Challenge 3

--- exercise-three.js
+++ exercise-three.js
@@ -1,10 +1,14 @@
 /*The sentence is: I Love Mangoes And Strawberries!*/
-string_to_array = function(str){
+string_to_array = function(str)
+{
   return str.trim().split(" ");
 };
 /*Array of String*/
-let arr = string_to_array("I Love Mangoes And Strawberries !");
-let arr = ['Vanilla','Strawberry','Butterscotch','Chocolate','Caramel'];
-for(let i=0;i<arr.length;i++){
+let arr = string_to_array("I Love Mangoes And Strawberries");
+console.log(arr);
+
+/*for loop to console log all of the elements plus the index and a comma*/
+for(let i=0;i<arr.length;i++)
+{
   console.log(i+','+arr[i]);
 }
