0f627f0fd9380ad590bbe54aa0b50d714339674a
sonalaggarwal555 <46764998+sonalaggarwal555@users.noreply.github.com>
2019-12-22 12:34:30 -0800

    Lesson-3 Assignment-2 Regular Challenge 2

--- exercise-two.js
+++ exercise-two.js
@@ -1,2 +1,10 @@
-let arr = ['I','Love','Mangoes','And','Strawberries','!'];
-console.log(arr[2]);
+/*The sentence is: I Love Mangoes And Strawberries!*/
+
+string_to_array = function(str){
+  return str.trim().split(" ");
+};
+/*Array of String*/
+let arr = string_to_array("I Love Mangoes And Strawberries!");
+
+/*Third word in array*/
+console.log('Third word in array: '+arr[2]);
